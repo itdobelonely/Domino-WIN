dc95736831c249ad620b0d46c86165acc4ed18e4
Zanatta <121562999+Zommodore@users.noreply.github.com>
2023-08-11 11:31:17 -0300

    Update Dom_WIN_Model.cpp

--- Dom_WIN_Model.cpp
+++ Dom_WIN_Model.cpp
@@ -4,5 +4,19 @@
 //Gustavo Nascimento Zanatta
 //Juan Fernando Coucolis Isano
 
-#include "Dom_WIN_Model.h"
+#include "DOM-WIN_Model.h"
+#include <stdio.h>
+
+
+typedef struct stpeca //Estrutura de dados contendo os lados do dominó e a situação atual do jogo.
+{
+    int lado1;
+    int lado2;
+    char status;//Representa diferentes estados do jogo
+
+} tipoPeca;
+
+tipoPeca peca[28];//Define a quantidade de peças 
+tipoPeca aux;
+
 
