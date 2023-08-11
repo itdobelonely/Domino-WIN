794fc374c94cf92b96bb4abeaa9ae118baec4199
Zanatta <121562999+Zommodore@users.noreply.github.com>
2023-08-11 11:32:29 -0300

    Update Dom_WIN_View.cpp

--- Dom_WIN_View.cpp
+++ Dom_WIN_View.cpp
@@ -4,4 +4,28 @@
 //Gustavo Nascimento Zanatta
 //Juan Fernando Coucolis Isano
 
-#include "Dom_WIN_view.h"
+#include "DOM-WIN_View.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int menuGeral()//Função para exibir o menu do jogo.
+{
+	int opm;
+	
+    printf ("\n\n1.Organizar pecas");
+    printf ("\n2.Embaralhar pecas");
+    printf ("\n9.Sair");
+    printf ("\nOpcao:");
+	scanf ("%d",&opm);
+	return opm;
+}
+
+void apresentaPecas()
+{
+    int i;
+	for (i=0;i<28;i++)
+    {
+        printf("(%d|%d)",peca[i].lado1,peca[i].lado2);//Mostra as peças de forma aleatória
+    }
+}
